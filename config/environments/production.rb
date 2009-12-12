0547432d497953fb816700f4b0ee620f005820a7
Simon Smith <supersi2000@hotmail.com>
2009-12-12 13:08:56 +0000

    Added the URL prefix for the production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,9 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
+# Prepend the calendar prefix to all urls
+config.action_controller.relative_url_root = "/calendar"
+
 # See everything in the log (default is :info)
 # config.log_level = :debug
 
