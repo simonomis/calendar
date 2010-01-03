ff948598f7a0febd40c5157bbad41666cec1427e
Simon Smith <supersi2000@hotmail.com>
2010-01-03 01:25:47 +0000

    Made the highlighting work with the colours. No BG highlighting still doesn't work though.

--- public/javascripts/event_calendar.js
+++ public/javascripts/event_calendar.js
@@ -5,13 +5,21 @@ document.observe('dom:loaded', function() {
 	$$("div[data-event-id].ec-event-bg").each(function(el) {
 		$(el).observe('mouseover', function() {
 			$$("div[data-event-id='"+$(this).readAttribute("data-event-id")+"'].ec-event-bg").each (function(el2) {
-				el2.setStyle({ backgroundColor: "#2EAC6A" });
+				el2.setStyle({ backgroundColor: highlight(el2.readAttribute("data-color")) });
 			});
 		});
 		$(el).observe('mouseout', function() {
 			$$("div[data-event-id='"+$(this).readAttribute("data-event-id")+"'].ec-event-bg").each (function(el2) {
-				el2.setStyle({ backgroundColor: "#9aa4ad" });
+				el2.setStyle({ backgroundColor: el2.readAttribute("data-color") });
 			});
+		});
+	});
+	$$("div.ec-event-no-bg").each(function(el) {
+		$(el).observe('mouseover', function() {
+			el.setStyle({ color: highlight(el.readAttribute("data-color")) });
+		});
+		$(el).observe('mouseout', function() {
+			el.setStyle({ color: el.readAttribute("data-color") });
 		});
 	});
 	
@@ -25,3 +33,17 @@ document.observe('dom:loaded', function() {
 		});
 	});
 });
+
+function highlight(color) {
+	if (color.charAt(0) == '#' && color.length == 7) {
+		/* it's a hex color string (i.e. '#rrggbb') so increase each color by 10% */
+		var r = parseInt(parseInt(color.substring(1,3), 16) * 1.1);
+		var g = parseInt(parseInt(color.substring(3,5), 16) * 1.1);
+		var b = parseInt(parseInt(color.substring(5,7), 16) * 1.1);
+		var str = "#" + ((Math.min(r,255) * 65536) + (Math.min(g,255) * 256) + Math.min(b,255)).toString(16);
+		return str;
+	}
+	
+	/* if we got this far we don't know what it is so just go green */
+	return "#2EAC6A";
+}
